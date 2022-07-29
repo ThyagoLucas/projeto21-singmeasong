a460cf5a561e4e3b9f86ba5f755e8faf2155be95
ThyagoLucas <salc@icomp.ufam.edu.br>
2022-07-29 01:47:21 -0400

    fix:added ESLINT on project

--- back-end/src/tests/app.test.ts
+++ back-end/src/tests/app.test.ts
@@ -1,29 +1,28 @@
-import supertest from "supertest";
-import app from "../app.js";
+import supertest from 'supertest';
 import { faker } from '@faker-js/faker';
-import { createEnvironment } from "./factore/createEnvironment.js";
+import app from '../app.js';
+import { createEnvironment } from './factore/createEnvironment.js';
 
-
-beforeAll( async ()=>{
-    await createEnvironment();
-})
+beforeAll(async () => {
+	await createEnvironment();
+});
 
 const RECOMMENDATION = {
-    name: faker.music.songName(),
-    youtubeLink: `https://www.youtube.com/watch?v=${faker.database.engine()}`
-}
+	name: faker.music.songName(),
+	youtubeLink: `https://www.youtube.com/watch?v=${faker.database.engine()}`,
+};
 
-describe('suit insert recommendations',()=>{
+describe('tests integrations suits', () => {
+	it('give a test register', async () => {
+		const response = await supertest(app).post('/recommendations/').send(RECOMMENDATION);
+		expect(response.statusCode).toBe(201);
+	});
 
-    it('give a test register', async ()=>{
-        const response = await supertest(app).post('/recommendations/').send(RECOMMENDATION);
-        expect(response.statusCode).toBe(201);
-        
-    })
+	it('try same name register', async () => {
+		await supertest(app).post('/recommendations/').send(RECOMMENDATION);
+		const trySameName = await supertest(app).post('/recommendations/').send(RECOMMENDATION);
+		expect(trySameName.statusCode).toBe(409);
+	});
 
-    it('try same name register',async ()=>{
-        await supertest(app).post('/recommendations/').send(RECOMMENDATION);
-        const trySameName = await supertest(app).post('/recommendations/').send(RECOMMENDATION);
-        expect(trySameName.statusCode).toBe(409);
-    })
-})
+	it('');
+});
--- back-end/src/tests/factore/createEnvironment.ts
+++ back-end/src/tests/factore/createEnvironment.ts
@@ -1,7 +1,26 @@
-import { prisma } from "../../database.js"
+import { faker } from '@faker-js/faker';
+import { Recommendation } from '@prisma/client';
+import { prisma } from '../../database.js';
 
-export async function createEnvironment(){
+export async function createEnvironment() {
+	const datas = [
+		{
+			name: faker.music.songName(),
+			youtubeLink: `https://www.youtube.com/watch?v=${faker.database.engine()}`,
+		},
 
-    await prisma.recommendation.deleteMany({});
+		{
+			name: faker.music.songName(),
+			youtubeLink: `https://www.youtube.com/watch?v=${faker.database.engine()}`,
 
+		}, {
+			name: faker.music.songName(),
+			youtubeLink: `https://www.youtube.com/watch?v=${faker.database.engine()}`,
+
+		} as Recommendation];
+
+	await prisma.recommendation.deleteMany({});
+	await prisma.recommendation.createMany({
+		data: datas,
+	});
 }
